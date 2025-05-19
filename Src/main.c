6466bf3f1c4ed45a4b127d66f4e8ecf6770799e5
ThienBao3617 <nltb12042003@gmail.com>
2025-05-20 06:00:17 +0700

    init tasks and enable faults

--- Src/main.c
+++ Src/main.c
@@ -27,8 +27,27 @@ void task4_handler(void);
 void init_systick_timer(uint32_t tick_hz);
 __attribute__((naked)) init_scheduler_task(uint32_t sched_top_of_stack);
 
+init_task_stack(void);
+enable_processor_faults(void);
+
+#define MAX_STACKS 4
+uint32_t psp_of_stacks[MAX_STACKS] = {T1_STACK_START, T2_STACK_START, T3_STACK_START, T4_STACK_START};
+uint32_t task_handlers[MAX_STACKS];
+
 int main (void){
+    enable_processor_faults();
     init_scheduler_task(SCHEDULER_STACK_START);
+
+    task_handlers[0] = (uint32_t)task1_handler;
+    task_handlers[1] = (uint32_t)task2_handler;
+    task_handlers[2] = (uint32_t)task3_handler;
+    task_handlers[3] = (uint32_t)task4_handler;
+
+    init_task_stack();
+
+    // convert from MSP to PSP
+    switch_msp_to_psp();
+
     init_systick_timer(TICK_HZ);
     //loop forever
     for(;;);
@@ -84,6 +103,55 @@ void init_systick_timer(uint32_t tick_hz){
 __attribute__((naked)) init_scheduler_task(uint32_t sched_top_of_stack){
     __asm volatile("MSR MSP, %0": : "r"(sched_top_of_stack):);
     __asm volatile("BX LR"); //copy value of LR into PC
+}
+
+#define DUMMY_XPSR 0x01000000
+init_task_stack(void){
+    uint32_t *pPSP;
+    for(int i = 0; i < MAX_STACKS; i++){
+        pPSP = (uint32_t*)psp_of_stacks[i];
+
+        pPSP--; // XPSR reg
+        *pPSP = DUMMY_XPSR;
+
+        pPSP--; // PC reg
+        *pPSP = task_handlers[i];
+
+        pPSP--; // LR reg
+        *pPSP = 0xFFFFFFFD;
+
+        for(int j = 0; j < 13; j++){
+            pPSP--;
+            *pPSP = 0;
+        }
+
+        // after done, save sPSP
+        psp_of_stacks[i] = (uint32_t)pPSP;
+    }
+}
+
+enable_processor_faults(void){
+    // 1 enable all config exception like usage fault, mem manage fault and bus fault
+    uint32_t *pSHCSR = (uint32_t*)0xE000ED24;
+
+    *pSHCSR |= (1 << 16); // mem manage fault
+    *pSHCSR |= (1 << 17); // bus fault
+    *pSHCSR |= (1 << 18); // usage fault
+}
+
+void HardFault_Handler(void){
+    printf("Exception: HardFault\n");
+    while(1);
+}
+
+void MemManageFault_Handler(void){
+    printf("Exception: MemManageFault\n");
+    while(1);
+}
+
+void BusFault_Handler(void){
+    printf("Exception: Busfault\n");
+    while(1);
 }
 
 void SysTick_Handler(){}
